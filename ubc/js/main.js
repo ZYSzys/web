59b9fe9014a04000b541f046d8798ea114a1d0c5
章永胜 <1220736035@qq.com>
2017-12-04 14:35:33 +0800

    add button;

--- ubc/js/main.js
+++ ubc/js/main.js
@@ -1,5 +1,6 @@
 var slideIndex = 0;
 var slides = document.getElementsByClassName("mySlides");
+var t;
 
 function reset() {
 	var header = document.getElementsByClassName("jumbotron");
@@ -7,6 +8,13 @@ function reset() {
 
 	for (var i = 0; i < slides.length; i++) {
 		slides[i].style.display = "none"; 
+	}
+
+	var imgs = document.getElementsByTagName("img");
+	for (var i = 0; i < imgs.length; i++) {
+		imgs[i].style.borderWidth = '';
+		imgs[i].style.borderColor = '';
+		imgs[i].style.borderStyle = ''; 
 	}
 
 	buttonShow();
@@ -20,6 +28,35 @@ function buttonShow() {
 	}
 }
 
+function prev() {
+	slideIndex--;
+	if(slideIndex < 0)
+		slideIndex = 7;
+	showOne(slideIndex);
+}
+
+function next() {
+	slideIndex++;
+	if(slideIndex > 7)
+		slideIndex = 0;
+	showOne(slideIndex);
+}
+
+function clr() {
+	var header = document.getElementsByClassName("jumbotron");
+	header[0].style.display = "block";
+	for (var i = 0; i < slides.length; i++) {
+		slides[i].style.width = "100%";
+		slides[i].style.display = "block"; 
+	}
+	var bt = document.getElementsByTagName("button");
+	for(var i = 0; i < bt.length-1; i++)
+	{
+		bt[i].style.display = "none";
+	}
+	clearTimeout(t);
+}
+
 function showSlides() {
 	reset();
 	slideIndex++;
@@ -30,11 +67,12 @@ function showSlides() {
 	slides[slideIndex-1].style.display = "block"; 
 
 	//console.log(slides.index);
-    setTimeout(showSlides, 2000); 
+    t = setTimeout(showSlides, 2000); 
 }
 
 function showOne(index) {
 	reset();
+	slideIndex = index;
 	slides[index].style.width = "400%";
 	slides[index].style.display = "block"; 
 }
