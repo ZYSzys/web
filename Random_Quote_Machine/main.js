26acb09a7eaa19ff99212fdd1b86b50903604ec3
ZYSzys <17367077526@163.com>
2018-05-21 10:47:49 +0800

    :imp: deal all cdn

--- Random_Quote_Machine/main.js
+++ Random_Quote_Machine/main.js
@@ -1,135 +1,130 @@
 var colors = [
-"#16a085",
-"#27ae60",
-"#2c3e50",
-"#f39c12",
-"#e74c3c",
-"#9b59b6",
-"#FB6964",
-"#342224",
-"#472E32",
-"#BDBB99",
-"#77B1A9",
-"#73A857"
+    "#16a085",
+    "#27ae60",
+    "#2c3e50",
+    "#f39c12",
+    "#e74c3c",
+    "#9b59b6",
+    "#FB6964",
+    "#342224",
+    "#472E32",
+    "#BDBB99",
+    "#77B1A9",
+    "#73A857"
 ];
-var currentQuote = "", currentAuthor = "";
+var currentQuote = "",
+    currentAuthor = "";
 
 function openURL(url) {
-	window.open(
-		url,
-		"Share",
-		"width=500,height=400,toobar=0,location=0,statusbar=0,menubar=0,resizebar=0"
-		);
+    window.open(
+        url,
+        "Share",
+        "width=500,height=400,toobar=0,location=0,statusbar=0,menubar=0,resizebar=0"
+    );
 }
 
 function inIframe() {
-	try {
-		return window.self !== window.top;
-	} catch(e) {
-		return true;
-	}
+    try {
+        return window.self !== window.top;
+    } catch (e) {
+        return true;
+    }
 }
 
 function getQuote() {
-	$.ajax({
-		headers: {
-			"X-Mashape-Key": "OivH71yd3tmshl9YKzFH7BTzBVRQp1RaKLajsnafgL2aPsfP9V",
-			Accept: "application/json",
-			"Content-Type": "application/x-www-form-urlencoded"
-		},
-		url: "https://andruxnet-random-famous-quotes.p.mashape.com/cat=",
-		success: function(r) {
-			if(typeof r == "string") {
-				r = JSON.parse(r);
-			}
-			currentQuote = r.quote;
-			currentAuthor = r.author;
-			if(inIframe()) {
-				$("#tweet-quote").attr(
-					"href",
-					"https://twitter.com/intent/tweet?hashtags=quotes&related=freecodecamp&text=" + encodeURIComponent('"'+currentQuote+'"'+currentAuthor)
-					);
-				$("#tumblr-quote").attr(
-					"href",
-					"https://www.tumblr.com/widgets/share/tool?posttype=quote&tags=quotes,freecodecamp&caption=" +
-					encodeURIComponent(currentAuthor) +
-					"&content=" +
-					encodeURIComponent(currentQuote) +
-					"&canonicalUrl=https%3A%2F%2Fwww.tumblr.com%2Fbuttons&shareSource=tumblr_share_button"
-					);
-			}
-			$(".quote-text").animate(
-			{
-				opacity: 0
-			},
-			500,
-			function () {
-				$(this).animate(
-				{
-					opacity: 1
-				},
-				500
-				);
-				$("#text").text(r.quote);
-			}
-			);
-			$(".quote-author").animate(
-			{
-				opacity: 0
-			},
-			500,
-			function() {
-				$(this).animate(
-				{
-					opacity: 1
-				},
-				500
-				);
-				$("#author").html(r.author);
-			}
-			);
+    $.ajax({
+        headers: {
+            "X-Mashape-Key": "OivH71yd3tmshl9YKzFH7BTzBVRQp1RaKLajsnafgL2aPsfP9V",
+            Accept: "application/json",
+            "Content-Type": "application/x-www-form-urlencoded"
+        },
+        url: "https://andruxnet-random-famous-quotes.p.mashape.com/cat=",
+        success: function(r) {
+            if (typeof r == "string") {
+                r = JSON.parse(r);
+            }
+            currentQuote = r.quote;
+            currentAuthor = r.author;
+            if (inIframe()) {
+                $("#tweet-quote").attr(
+                    "href",
+                    "https://twitter.com/intent/tweet?hashtags=quotes&related=freecodecamp&text=" + encodeURIComponent('"' + currentQuote + '"' + currentAuthor)
+                );
+                $("#tumblr-quote").attr(
+                    "href",
+                    "https://www.tumblr.com/widgets/share/tool?posttype=quote&tags=quotes,freecodecamp&caption=" +
+                    encodeURIComponent(currentAuthor) +
+                    "&content=" +
+                    encodeURIComponent(currentQuote) +
+                    "&canonicalUrl=https%3A%2F%2Fwww.tumblr.com%2Fbuttons&shareSource=tumblr_share_button"
+                );
+            }
+            $(".quote-text").animate({
+                    opacity: 0
+                },
+                500,
+                function() {
+                    $(this).animate({
+                            opacity: 1
+                        },
+                        500
+                    );
+                    $("#text").text(r.quote);
+                }
+            );
+            $(".quote-author").animate({
+                    opacity: 0
+                },
+                500,
+                function() {
+                    $(this).animate({
+                            opacity: 1
+                        },
+                        500
+                    );
+                    $("#author").html(r.author);
+                }
+            );
 
-			var color = Math.floor(Math.random()*colors.length);
-			$("html body").animate(
-			{
-				backgroundColor: colors[color],
-				color: colors[color]
-			},
-			1000
-			);
-			$(".button").animate(
-			{
-				backgroundColor: colors[color]
-			},
-			1000
-			);
-		}
-	});
+            var color = Math.floor(Math.random() * colors.length);
+            $("html body").animate({
+                    backgroundColor: colors[color],
+                    color: colors[color]
+                },
+                1000
+            );
+            $(".button").animate({
+                    backgroundColor: colors[color]
+                },
+                1000
+            );
+        }
+    });
 }
 
 $(document).ready(function() {
-	getQuote();
-	$("#new-quote").on("click", getQuote);
-	$("#tweet-quote").on("click", function() {
-		if(!inIframe()) {
-			openURL(
-				"https://twitter.com/intent/tweet?hashtags=quotes&related=freecodecamp&text=" + 
-				encodeURIComponent('"' + currentQuote + '" ' + currentAuthor)
-				);
-		}
-		//code
-	});
-	$("#tumblr-quote").on("click", function() {
-		if(!inIframe()) {
-			openURL(
-				"https://www.tumblr.com/widgets/share/tool?posttype=quote&tags=quotes,freecodecamp&caption=" +
-				encodeURIComponent(currentAuthor) +
-				"&content=" +
-				encodeURIComponent(currentQuote) +
-				"&canonicalUrl=https%3A%2F%2Fwww.tumblr.com%2Fbuttons&shareSource=tumblr_share_button"
-				);
-		}
-		//code
-	});
+    getQuote();
+    $("#new-quote").on("click", getQuote);
+    $("#tweet-quote").on("click", function() {
+        if (!inIframe()) {
+            openURL(
+                "https://twitter.com/intent/tweet?hashtags=quotes&related=freecodecamp&text=" +
+                encodeURIComponent('"' + currentQuote + '" ' + currentAuthor)
+            );
+        }
+        //code
+    });
+    $("#tumblr-quote").on("click", function() {
+        if (!inIframe()) {
+            openURL(
+                "https://www.tumblr.com/widgets/share/tool?posttype=quote&tags=quotes,freecodecamp&caption=" +
+                encodeURIComponent(currentAuthor) +
+                "&content=" +
+                encodeURIComponent(currentQuote) +
+                "&canonicalUrl=https%3A%2F%2Fwww.tumblr.com%2Fbuttons&shareSource=tumblr_share_button"
+            );
+        }
+        //code
+    });
 
 });
